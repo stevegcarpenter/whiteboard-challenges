38006147938b04e84b1faa26650fa14d367e5cd6
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-18 14:36:43 -0800

    Removed result array since it already returns a filter

--- whiteboard-challenge-04/lib/solution.js
+++ whiteboard-challenge-04/lib/solution.js
@@ -4,7 +4,6 @@ const solution = module.exports = {};
 
 solution.findIntersections = (a, b) => {
   var lut = {};
-  var result = [];
   a.map(e => lut[e] = true);
   return b.filter(e => lut[e]);
 };
