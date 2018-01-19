c40a4b5054c280c6e07e79ad97408472a2e250de
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-18 19:43:16 -0800

    Added a check for invalid arguments

--- whiteboard-challenge-04/lib/solution.js
+++ whiteboard-challenge-04/lib/solution.js
@@ -4,6 +4,7 @@ const solution = module.exports = {};
 
 solution.findIntersections = (a, b) => {
   var lut = {};
+  if (!Array.isArray(a) || !Array.isArray(b)) return [];
   a.map(e => lut[e] = true);
   return b.filter(e => lut[e]);
 };
