92dce3f660002f2dd1063bb3fd9dce39b1bb9d6b
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-26 17:56:21 -0800

    Tests added for the checkBraces function

--- whiteboard-challenge-10/__test__/solution.test.js
+++ whiteboard-challenge-10/__test__/solution.test.js
@@ -1,8 +1,29 @@
 'use strict';
 
-const findNthFromEnd = require('../lib/solution');
-const Node = require('../lib/nd');
+const solution = require('../lib/solution');
 
 describe('Solution Module', () => {
-
+  describe('#checkBraces', () => {
+    it('should return true if the input has no braces at all', () => {
+      expect(solution.checkBraces('')).toBe(true);
+      expect(solution.checkBraces('this technically has a perfect matching number of braces')).toBe(true);
+    });
+    it('should return true if their are the perfect number of braces.', () => {
+      expect(solution.checkBraces('{ { { { {}{}}{}}}}')).toBe(true);
+    });
+    it('should return false if there are too many opening braces.', () => {
+      expect(solution.checkBraces('{{{{{{{}')).toBe(false);
+    });
+    it('should return false if there are too many closing braces.', () => {
+      expect(solution.checkBraces('{}}}}}}}}')).toBe(false);
+    });
+    it('should throw an error if the input argument is not a string.', () => {
+      expect(() => solution.checkBraces(0)).toThrow('Ooops');
+      expect(() => solution.checkBraces(1)).toThrow('Ooops');
+      expect(() => solution.checkBraces({})).toThrow('Ooops');
+      expect(() => solution.checkBraces(null)).toThrow('Ooops');
+      expect(() => solution.checkBraces(undefined)).toThrow('Ooops');
+      expect(() => solution.checkBraces(NaN)).toThrow('Ooops');
+    });
+  });
 });
