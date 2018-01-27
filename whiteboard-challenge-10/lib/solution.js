2845d674e2b088651ad7d10761bcc51354db5c4e
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-26 17:56:04 -0800

    Implemented the checkBraces function

--- whiteboard-challenge-10/lib/solution.js
+++ whiteboard-challenge-10/lib/solution.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const Node = require('./nd');
+const solution = module.exports = {};
 
-module.exports = () => {
-
+solution.checkBraces = (squigleys) => {
+  let stack = 0;
+  // squigleys must be a string
+  if (typeof squigleys !== 'string') throw new Error('Ooops');
+  for (let i = 0; i < squigleys.length; i++) {
+    if (squigleys[i] === '{') stack++;
+    else if (squigleys[i] === '}') {
+      // this denotes a closing brace when no opening one existed
+      if (stack === 0) return false;
+      stack--;
+    }
+  }
+  // this denotes too many opening braces
+  if (stack) return false;
+  // a perfect match of braces
+  return true;
 };
