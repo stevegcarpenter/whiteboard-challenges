702e47f2056aa1caed2071471d554e0d636728c7
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-29 14:20:47 -0800

    Added solution code

--- whiteboard-challenge-11/lib/solution.js
+++ whiteboard-challenge-11/lib/solution.js
@@ -1,5 +1,8 @@
 'use strict';
 
-module.exports = () => {
+module.exports = (arr) => {
+  if (!arr) throw new Error('Error: arr must be falsey');
+  if (!arr.length) throw new Error('Error: arr len must be positive');
 
+  return arr.reduce((acc, cur) => acc - cur, 5050);
 };
