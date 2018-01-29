495f185794b941b8bb55065b1bef50bccd042d84
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-29 14:21:02 -0800

    Added tests for the calculateMissing function

--- whiteboard-challenge-11/__test__/solution.test.js
+++ whiteboard-challenge-11/__test__/solution.test.js
@@ -1,8 +1,21 @@
 'use strict';
 
-const solution = require('../lib/solution');
+const calculateMissing = require('../lib/solution');
 
 describe('Solution Module', () => {
-  describe('#', () => {
+  describe('#calculateMissing', () => {
+    let arr = [...Array(100)].map((e, i) => e = i + 1);
+    arr.splice(9, 1);
+    it('should indicate the missing number is 10', () => {
+      expect(calculateMissing(arr)).toBe(10);
+    });
+
+    it('should throw an error if the array is falsey', () => {
+      expect(() => calculateMissing(null).toThrow('Error: arr must be falsey'));
+    });
+
+    it('should throw an error if the array does not have 100 items.', () => {
+      expect(() => calculateMissing([])).toThrow('Error: arr len must be positive');
+    });
   });
 });
