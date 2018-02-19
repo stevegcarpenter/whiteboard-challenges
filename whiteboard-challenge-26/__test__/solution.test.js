2f728d41125f224e1956b64523f9cbeee58a0b9e
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-19 13:37:43 -0800

    Adding scaffolding for whiteboard solution code

---
+++ whiteboard-challenge-26/__test__/solution.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const utils = require('../lib/utils');
+
+describe('Utils Module', () => {
+  describe('#map', () => {
+    it('should ...', () => {
+
+    });
+
+    it('should ...', () => {
+
+    });
+  });
+});
