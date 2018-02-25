2c9345200a9f3f4eb090535476ba3046680da320
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-25 10:13:31 -0800

    Adding untested sort anagrams code

--- whiteboard-challenge-29/lib/solution.js
+++ whiteboard-challenge-29/lib/solution.js
@@ -1,4 +1,27 @@
-module.exports = (str) => {
-  const idx = str.split(' ').map(w => w.split('').reduce((s, l) => s + l.charCodeAt(0) - 96, 0)).reduce((highscore, curscore, idx, arr) => curscore > arr[highscore] ? idx : highscore, 0);
-  return str.split(' ')[idx];
+// sort anagrams alphabetically
+module.exports = (wordsArr) => {
+  if (!wordsArr || !(wordsArr instanceof Array))
+    throw new TypeError('wordsArr is invalid');
+
+  let anagrams = {};
+
+  // First, add all anagrams into an object based off their key value
+  wordsArr.map(word => {
+    if (typeof word !== 'string')
+      throw new TypeError(`${word} is not a string`);
+
+    let key = word.trim().toLower().split('').sort().join();
+
+    // Store anagrams in an array
+    if (!anagrams[key])
+      anagrams[key] = [];
+
+    anagrams[key].push(word.trim().toLower());
+  });
+
+  // Sort the keys and add all the anagrams back into the results array
+  let results = [];
+  Object.keys(anagrams).sort().map(key => results.concat(...anagrams[key]));
+
+  return results;
 };
