fcd3429b907084d278d47201088cee6bb34fcd97
Steven Carpenter <stevegcarpenter@gmail.com>
2018-03-05 18:43:25 -0800

    Adding solution and some additional tests

--- whiteboard-challenge-36/index.js
+++ whiteboard-challenge-36/index.js
@@ -1,21 +1,36 @@
 const TreeNode = require('./lib/bst').TreeNode;
 const BinarySearchTree = require('./lib/bst').BinarySearchTree;
+const areBstsAlike = require('./lib/solution');
 
-let nodes = [
+let nodesA = [
+  new TreeNode(4),
+  new TreeNode(2.5),
+  new TreeNode(1),
+  new TreeNode(3),
+  new TreeNode(6.5),
+  new TreeNode(7),
+  new TreeNode(5),
+];
+let bstA = new BinarySearchTree();
+for (let node of nodesA) {
+  bstA.insert(node);
+}
+
+let nodesB = [
   new TreeNode(4),
   new TreeNode(2),
   new TreeNode(1),
   new TreeNode(3),
   new TreeNode(6),
   new TreeNode(7),
-  new TreeNode(9),
+  new TreeNode(5),
 ];
-let bst = new BinarySearchTree();
-for (let node of nodes) {
-  bst.insert(node);
+let bstB = new BinarySearchTree();
+for (let node of nodesB) {
+  bstB.insert(node);
 }
 
-bst.isBalanced();
+areBstsAlike(bstA, bstB);
 
 // Build the following tree
 //                       7
--- whiteboard-challenge-36/lib/solution.js
+++ whiteboard-challenge-36/lib/solution.js
@@ -1,4 +1,36 @@
 'use strict';
 
-module.exports = () => {
+const BST = require('./bst').BinarySearchTree;
+
+// Check for tree likeness
+// This means only the structure needs to match
+// Node values are irrelevant
+module.exports = (bstA, bstB) => {
+  if (!bstA || !bstB)
+    throw new Error('both trees must be valid');
+  if (!(bstA instanceof BST) || !(bstB instanceof BST))
+    throw new TypeError('both trees must be BinarySearchTrees');
+
+  return recurseForAlikeness(bstA.root, bstB.root);
 };
+
+function recurseForAlikeness(nodeA, nodeB) {
+  // At least one node is valid
+  if (nodeA || nodeB) {
+    // They both must be valid
+    if (!nodeA) return false;
+    if (!nodeB) return false;
+  }
+  // Both nodes are null
+  if (!nodeA && !nodeB) {
+    return true;
+  }
+
+  // Check left sub-tree
+  if (!recurseForAlikeness(nodeA.left, nodeB.left)) return false;
+  // Check right sub-tree
+  if (!recurseForAlikeness(nodeA.right, nodeB.right)) return false;
+
+  // trees are alike
+  return true;
+}
